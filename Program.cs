c77ce304a1ff790a441fa8bdbceac67b1f268931
Akash santhosh <akash777kk@gmail.com>
2023-09-11 10:31:46 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringImmutable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string s = "haloo";//heap id 1001 destroyed with garbage collection ,remove from stack
+            Console.WriteLine(s.GetHashCode());
+            s = "hii";//heap id 2002
+            Console.WriteLine(s.GetHashCode());
+            Console.WriteLine("---------");
+
+            //state can change
+            StringBuilder str= new StringBuilder("haloooo");//using stack id same can diffrent value
+            Console.WriteLine(str.GetHashCode());
+            str.Append("hiiii");
+            Console.WriteLine(str.GetHashCode());
+
+
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("StringImmutable")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StringImmutable")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9ada07ac-0dd8-4dc6-9fec-8dd7f3d97d71")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
